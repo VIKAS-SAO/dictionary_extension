3a02bbbada5ac0d903f6ebe1c07262dff86d7561
Vikas Sao <51861426+VIKAS-SAO@users.noreply.github.com>
2020-07-29 17:31:17 +0530

    second commit !

--- popup/index.js
+++ popup/index.js
@@ -7,22 +7,24 @@ input.textContent = background.word;
 
 async function  fetcher(){
     let url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/' + background.word + '?key=d244f8b8-b483-489f-93d2-7ef1b638b823';
-    //   fetch(url ).then(function(response ){
-    //     response.json().then(function(data){
-    //         console.log(data)
-    //         definition.textContent =data;
-    //     })
+      fetch(url ).then(function(response ){
+        response.json().then(function(data){
+            console.log(data)
+            definition.textContent =data[0].shortdef[0];
+        })
         
-    // } )
+    } )
 
-    let response = await  fetch(url);
-    let data = await response.json()
-    console.log(data)
-    definition.textContent =data[0].shortdef[0];
+    // let response = await  fetch(url);
+    // let data = await response.json()
+    // console.log(data)
+    // definition.textContent =data[0].shortdef[0];
 
 }
- fetcher()
- 
+setTimeout(function(){
+    fetcher();
+},200)
+  
 
 
 
